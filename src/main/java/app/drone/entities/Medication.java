4db8ec3891ecd74091f15c86a90976a2877fd1c4
Hamlet <hamletarencibia@gmail.com>
2023-02-24 16:22:26 -0500

    chore(entity): add constraints to medication

--- src/main/java/app/drone/entities/Medication.java
+++ src/main/java/app/drone/entities/Medication.java
@@ -3,27 +3,44 @@ package app.drone.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 public class Medication {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@NotNull(message = "You must provide a name.")
+	@Pattern(regexp = "^([0-9A-Za-z\\-_]+)$", message = "Invalid name. Allowed only letters, numbers, '-', '_'.")
+	private String name;
+	@Min(value = 1, message = "The weight must be greater than 0.")
 	private float weight;
+	@NotNull(message = "You must provide a code.")
+	@Pattern(regexp = "^([0-9A-Z_]+)$", message = "Invalid code. Allowed only upper case letters, underscore and numbers.")
 	private String code;
+	@NotNull(message = "You must provide an image.")
 	private String image;
 
 	public Medication() {
 	}
 
-	public Medication(float weight, String code, String image) {
+	public Medication(String name, @Min(value = 1, message = "The weight must be greater than 0.") float weight,
+			String code, String image) {
+		super();
+		this.name = name;
 		this.weight = weight;
 		this.code = code;
 		this.image = image;
 	}
 
-	public Long getId() {
-		return id;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public float getWeight() {
@@ -49,4 +66,9 @@ public class Medication {
 	public void setImage(String image) {
 		this.image = image;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
 }
